81c2c91213eab19b2c0c14b9b7759b3daa072cdc
jaybonthius <46472915+jaybonthius@users.noreply.github.com>
2024-10-25 20:53:21 +0000

    render pm files on save

--- scripts/filesaver.sh
+++ scripts/filesaver.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")/.." || exit
 
 # Watch for changes in *.rkt and *.p files in both shindig and content directories
 inotifywait -m -e modify,create,delete -r --format '%w%f' shindig content | while read -r file; do
-    if [[ "$file" == *.rkt || "$file" == *.p ]]; then
+    if [[ "$file" == *.rkt || "$file" == *.p || "$file" == *.pm ]]; then
         echo "Change detected in $file"
         
         # If the change is in a .rkt file or in the shindig directory
@@ -16,14 +16,16 @@ inotifywait -m -e modify,create,delete -r --format '%w%f' shindig content | whil
         fi
         
         # If the change is in a .p file
-        if [[ "$file" == *.p ]]; then
+        if [[ "$file" == *.p || "$file" == *.pm ]]; then
             # Call raco pollen render for the changed .p file
             raco pollen render "$file"
             echo "Rendered $file using raco pollen"
             
-            # "Save" all *.poly.pm files in the directory of the changed file
-            find "$(dirname "$file")" -name "*.pm" -type f -exec touch {} +
-            echo "All *.pm files in $(dirname "$file") have been 'saved'"
+            if [[ "$file" == *.p ]]; then
+                # "Save" all *.poly.pm files in the directory of the changed file
+                find "$(dirname "$file")" -name "*.pm" -type f -exec touch {} +
+                echo "All *.pm files in $(dirname "$file") have been 'saved'"
+            fi
         fi
     fi
 done
