e2d00a2b8af0b2317c1a31c4260e4a092eaaa2ba
Jay <jaybonthius@gmail.com>
2024-12-07 19:38:21 -0800

    add index back to pagetree

--- content/config.rkt
+++ content/config.rkt
@@ -3,7 +3,8 @@
 (provide (all-defined-out))
 
 (define pagetree
-  '(pagetree (frontmatter acknowledgements)
+  '(pagetree index 
+            (frontmatter acknowledgements)
              (chapter (understanding-the-derivative index how-do-we-measure-velocity))
              (backmatter book-idx)))
 
